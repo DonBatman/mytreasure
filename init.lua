090eb62e8a92182754d5e2a62f17129385f9b064
DonBatman <serfdon@gmail.com>
2026-01-25 05:29:06 -0800

    Added Lucky Block Support

--- init.lua
+++ init.lua
@@ -15,3 +15,21 @@ end
 if special_treasure then
 dofile(minetest.get_modpath("mytreasure").."/special.lua")
 end
+
+if core.get_modpath("lucky_block") then
+	lucky_block:add_blocks({
+		{"dro", {"mytreasure:sunken1"}, 24},
+		{"dro", {"mytreasure:sunken2"}, 24},
+		{"dro", {"mytreasure:sunken3"}, 24},
+		{"dro", {"mytreasure:dungeon"}, 24},
+		{"dro", {"mytreasure:dungeon2"}, 24},
+		{"dro", {"mytreasure:wool"}, 24},
+		{"dro", {"mytreasure:desert"}, 24},
+		{"dro", {"mytreasure:buried1"}, 24},
+		{"dro", {"mytreasure:buried2"}, 24},
+		{"dro", {"mytreasure:buried3"}, 24},
+		{"dro", {"mytreasure:cave1"}, 24},
+		{"dro", {"mytreasure:cave2"}, 24},
+		{"dro", {"mytreasure:cave3"}, 24},
+	})
+end
